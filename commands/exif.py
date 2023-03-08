7d1715f0cef6b1912f3532e6c405c19f6180bae4
ItsSynergi <Michael_Leng@outlook.com>
2023-03-08 01:35:10 -0800

    Implement MIE generation

--- commands/exif.py
+++ commands/exif.py
@@ -1,3 +1,5 @@
+import os
+
 from util.helpers import Util
 from util.exiftool import ExifTool
 
@@ -15,3 +17,23 @@ class Exif:
             Util.verify_extension_exists(exiftool, self.extension, self.directory)
 
             Util.create_directory_or_abort(self.output_directory)
+
+            self.do_exif(exiftool)
+
+    def do_exif(self, exiftool: ExifTool):
+        file_names = Util.get_valid_file_names(exiftool, self.extension, self.directory)
+
+        for file_name in file_names:
+            file_name_extensionless = os.path.splitext(file_name)[0]
+            full_path_to = f"{self.output_directory}/{file_name_extensionless}.mie"
+
+            print(f"Writing MIE binary dump to [{full_path_to}]...")
+
+            exiftool.execute_with_extension(
+                self.extension,
+                "-o",
+                full_path_to,
+                "-all:all",
+                "-icc_profile",
+                f"{self.directory}/{file_name}"
+            )
