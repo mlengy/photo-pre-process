382c1ab1b9b377e21297689cc1f8362490de025e
ItsSynergi <Michael_Leng@outlook.com>
2023-03-07 17:48:01 -0800

    Implement directory helpers

--- util.py
+++ util.py
@@ -1,9 +1,16 @@
+import os
 import typer
 
 
 class Util:
-    def verify_directory(self):
-        pass
+    @staticmethod
+    def verify_directory(directory):
+        if not os.path.isdir(directory):
+            print(f"Directory {directory} does not exist!")
+            raise typer.Exit(code=1)
 
-    def verify_or_create_directory(self):
-        pass
+    @staticmethod
+    def verify_or_create_directory(directory):
+        if not os.path.isdir(directory):
+            print(f"Creating directory {directory} since it does not exist!")
+            os.makedirs(directory)
