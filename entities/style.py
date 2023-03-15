211b29ea530607fabd5428545eb903b93626033c
ItsSynergi <Michael_Leng@outlook.com>
2023-03-14 18:16:14 -0700

    Update styles

--- entities/style.py
+++ entities/style.py
@@ -4,28 +4,31 @@ from enum import Enum
 class Style(Enum):
     none = 00
 
-    # nature
-    nature = 10
+    # natural
+    natural = 10
     landscape = 11
     astro = 12
     flora = 13
     fauna = 14
     water = 15
 
-    # still
-    still = 20
+    # artificial
+    artificial = 20
     city = 21
     architecture = 22
-    road = 23
+    transport = 23
+    road = 24
 
     # people
     people = 30
     portrait = 31
     street = 32
     environmental = 33
-    event = 34
+    adventure = 34
+    event = 35
 
     # other
     other = 90
-    food = 91
-    product = 92
+    abstract = 91
+    food = 92
+    product = 93
