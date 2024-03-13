df35baa858821f4955bcc53cce62703a23c04537
ItsSynergi <Michael_Leng@outlook.com>
2024-03-13 11:11:26 -0700

    Bump version to 0.3.0 🎉

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="photo-pre-process",
     description="Scripts to pre-process digital media for storage and editing.",
-    version="0.2.1",
+    version="0.3.0",
     author="Michael Leng",
     author_email="michael@len.gy",
     url="https://len.gy",
